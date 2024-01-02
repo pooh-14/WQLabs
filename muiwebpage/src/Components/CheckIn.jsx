7f6e9da919b3afeecf2b4a2420a1618010778e38
pooh-14 <poojavetal14@gmail.com>
2024-01-02 02:25:56 -0800

    updates

--- muiwebpage/src/Components/CheckIn.jsx
+++ muiwebpage/src/Components/CheckIn.jsx
@@ -16,20 +16,24 @@ import {
 import DrawerComp from "./DrawerComp";
 import { AuthContext } from "./Context/AuthContext";
 import AddAPhotoOutlinedIcon from "@mui/icons-material/AddAPhotoOutlined";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
 const CheckIn = () => {
   const { menu, setMenu } = useContext(AuthContext);
   const [activeStep, setActiveStep] = useState(0);
 
   const containr = {
-    width: "85.5%",
+    width: { xs: "100%", md: "85.5%" },
     border: "1px solid #e4e4e5",
     margin: "auto",
     display: "flex",
+    justifyContent: "space-between",
     padding: "15px",
     marginTop: "32px",
     borderRadius: "10px",
     backgroundColor: "white",
+    flexDirection: { xs: "column", md: "row" },
+    alignItems:{sm:"center", md:"start"}
   };
 
   const photo = {
@@ -43,6 +47,8 @@ const CheckIn = () => {
     flexDirection: "column",
     justifyContent: "center",
   };
+
+
 
   return (
     <Box sx={{ display: "flex", backgroundColor: "#f5f5f6" }}>
@@ -66,25 +72,58 @@ const CheckIn = () => {
         <Box>
           <Navbar menu={menu} setMenu={setMenu} />
         </Box>
-        <Box sx={{ fontFamily: "Poppins" }}>
-          <Box sx={{ marginTop: "32px" }}>
-            <Stepper
-              sx={{ width: "69%", margin: "auto" }}
+        <Box sx={{ fontFamily: "Poppins", height:"100%" }}>
+          <Box sx={{ marginTop: "32px",display:{xs:"block",sm:"none"} }}>
+          <Stepper alternativeLabel  
+              sx={{ width: {xs:"97%",md:"69.5%"}, margin: "auto", }}
               activeStep={activeStep}
             >
-              <Step>
-                <StepLabel>Visitor CheckIn</StepLabel>
+              <Step >
+                <StepLabel>
+                  <Typography  sx={{fontSize:{xs:"12px",sm:"20px", md:"15px"}}}>Visitor CheckIn</Typography>
+                </StepLabel>
               </Step>
               <Step>
-                <StepLabel>Vehicle CheckIn</StepLabel>
+                <StepLabel>
+                  <Typography  sx={{fontSize:{xs:"12px",sm:"20px", md:"15px"}}}>Vehicle CheckIn</Typography>
+                </StepLabel>
               </Step>
               <Step>
-                <StepLabel>Visitor Details</StepLabel>
+                <StepLabel>
+                  <Typography  sx={{fontSize:{xs:"12px",sm:"20px", md:"15px"}}}>Visitor Details</Typography>
+                </StepLabel>
+              </Step>
+            </Stepper>  
+          </Box>
+          <Box sx={{ marginTop: "32px",display:{xs:"none",sm:"block"} }}>
+          <Stepper linear 
+              sx={{ width: {xs:"97%",md:"69.5%"}, margin: "auto", }}
+              activeStep={activeStep}
+            >
+              <Step >
+                <StepLabel>
+                  <Typography  sx={{fontSize:{xs:"12px",sm:"20px", md:"15px"}}}>Visitor CheckIn</Typography>
+                </StepLabel>
+              </Step>
+              <Step>
+                <StepLabel>
+                  <Typography  sx={{fontSize:{xs:"12px",sm:"20px", md:"15px"}}}>Vehicle CheckIn</Typography>
+                </StepLabel>
+              </Step>
+              <Step>
+                <StepLabel>
+                  <Typography  sx={{fontSize:{xs:"12px",sm:"20px", md:"15px"}}}>Visitor Details</Typography>
+                </StepLabel>
               </Step>
             </Stepper>
           </Box>
           <Box sx={containr}>
-            <Box sx={{ width: "29.5%", textAlign: "center" }}>
+            <Box
+              sx={{
+                width: { xs: "100%", sm: "50%", md: "29.5%" },
+                textAlign: "center",
+              }}
+            >
               <Box sx={photo}>
                 <AddAPhotoOutlinedIcon
                   sx={{ color: "#cccccc", fontSize: "70px" }}
@@ -95,68 +134,225 @@ const CheckIn = () => {
                   Take Photo
                 </Typography>
               </Box>
-              <Button
+              <Button 
                 variant="contained"
                 sx={{
                   marginTop: "17px",
                   textTransform: "none",
                   padding: "6px 8px",
                   backgroundColor: "#3e60d5",
+                  marginBottom: "2px",
+                  fontSize:{xs:"17px",sm:"20px", md:"14px"},
                 }}
               >
                 Open Camera
               </Button>
             </Box>
-            <Box sx={{ width: "70%", border: "1px solid blue" }}>
-              <FormControl>
-                <FormLabel>First Name</FormLabel>
-                <TextField
-                  placeholder="First Name"
-                  InputProps={{ sx: { width: "180px" } }}
-                />
+            <Box sx={{ width: { xs: "100%", md: "67%" },marginTop:{xs:"30px", md:"0"} }}>
+              <FormControl sx={{ width: "100%" }}>
+                <Grid container spacing={2}>
+                  <Grid
+                    item
+                    sx={{ display: "flex", flexDirection: "column" }}
+                    xs={12}
+                    sm={12}
+                    md={4}
+                  >
+                    <FormLabel
+                      sx={{
+                        fontSize: {sm:"20px",md:"15px"},
+                        fontWeight: "600",
+                        color: "#3C3C3C",
+                        marginBottom: "7px",
+                      }}
+                      required
+                    >
+                      First Name
+                    </FormLabel>
+                    <TextField
+                      InputProps={{ sx: { height: "37px", fontSize:{sm:"18px", md:"14px"} } }}
+                      placeholder="First Name"
+                    />
+                  </Grid>
 
-                <FormLabel>Middle Name</FormLabel>
-                <TextField
-                  placeholder="Middle Name"
-                  InputProps={{ sx: { width: "180px" } }}
-                />
+                  <Grid
+                    item
+                    sx={{ display: "flex", flexDirection: "column" }}
+                    xs={12}
+                    sm={6}
+                    md={4}
+                  >
+                    <FormLabel
+                      sx={{
+                        fontSize: {sm:"20px",md:"15px"},
+                        fontWeight: "600",
+                        color: "#3C3C3C",
+                        marginBottom: "7px",
+                      }}
+                      required
+                    >
+                      Middle Name
+                    </FormLabel>
+                    <TextField
+                      InputProps={{ sx: { height: "37px", fontSize:{sm:"18px", md:"14px"} } }}
+                      placeholder="Middle Name"
+                    />
+                  </Grid>
 
-                <FormLabel>Last Name</FormLabel>
-                <TextField
-                  placeholder="Last Name"
-                  InputProps={{ sx: { width: "180px" } }}
-                />
+                  <Grid
+                    item
+                    sx={{ display: "flex", flexDirection: "column" }}
+                    xs={12}
+                    sm={6}
+                    md={4}
+                  >
+                    <FormLabel
+                      sx={{
+                        fontSize: {sm:"20px",md:"15px"},
+                        fontWeight: "600",
+                        color: "#3C3C3C",
+                        marginBottom: "7px",
+                      }}
+                      required
+                    >
+                      Last Name
+                    </FormLabel>
+                    <TextField
+                      InputProps={{ sx: { height: "37px", fontSize:{sm:"18px", md:"14px"} } }}
+                      placeholder="Last Name"
+                    />
+                  </Grid>
+                </Grid>
 
-                <FormLabel>Phone</FormLabel>
-                <TextField
-                  placeholder="Phone"
-                  InputProps={{ sx: { width: "280px" } }}
-                />
+                <Grid container spacing={2}>
+                  <Grid
+                    item
+                    sx={{ display: "flex", flexDirection: "column" }}
+                    xs={12}
+                    sm={6}
+                    md={6}
+                  >
+                    <FormLabel
+                      sx={{
+                        fontSize: {sm:"20px",md:"15px"},
+                        fontWeight: "600",
+                        color: "#3C3C3C",
+                        marginBottom: "7px",
+                        marginTop: {xs:"0px",sm:"22px",md:"22px"},
+                      }}
+                      required
+                    >
+                      Phone
+                    </FormLabel>
+                    <TextField
+                      InputProps={{ sx: { height: "37px", fontSize:{sm:"18px", md:"14px"} } }}
+                      placeholder="Phone"
+                    />
+                  </Grid>
 
-                <FormLabel>Email</FormLabel>
-                <TextField
-                  placeholder="Email"
-                  InputProps={{ sx: { width: "280px" } }}
-                />
+                  <Grid
+                    item
+                    sx={{ display: "flex", flexDirection: "column" }}
+                    xs={12}
+                    sm={6}
+                    md={6}
+                  >
+                    <FormLabel
+                      sx={{
+                        fontSize: {sm:"20px",md:"15px"},
+                        fontWeight: "600",
+                        color: "#3C3C3C",
+                        marginBottom: "7px",
+                        marginTop: {xs:"0px",sm:"22px",md:"22px"},
+                      }}
+                      required
+                    >
+                      Email
+                    </FormLabel>
+                    <TextField
+                      InputProps={{ sx: { height: "37px", fontSize:{sm:"18px", md:"14px"} } }}
+                      placeholder="Email"
+                    />
+                  </Grid>
+                </Grid>
 
-                <FormLabel>Company</FormLabel>
-                <TextField
-                  placeholder="Company"
-                  InputProps={{ sx: { width: "280px" } }}
-                />
+                <Grid container spacing={2}>
+                  <Grid
+                    item
+                    sx={{ display: "flex", flexDirection: "column" }}
+                    xs={12}
+                    sm={6}
+                    md={6}
+                  >
+                    <FormLabel
+                      sx={{
+                        fontSize: {sm:"20px",md:"15px"},
+                        fontWeight: "600",
+                        color: "#3C3C3C",
+                        marginBottom: "7px",
+                        marginTop: {xs:"0px",sm:"22px",md:"22px"},
+                      }}
+                      required
+                    >
+                      Company
+                    </FormLabel>
+                    <TextField
+                      InputProps={{ sx: { height: "37px", fontSize:{sm:"18px", md:"14px"} } }}
+                      placeholder="Company"
+                    />
+                  </Grid>
 
-                <FormLabel>Contact Person</FormLabel>
-                <TextField
-                  placeholder="Contact Person"
-                  InputProps={{ sx: { width: "280px" } }}
-                />
+                  <Grid
+                    item
+                    sx={{ display: "flex", flexDirection: "column" }}
+                    xs={12}
+                    sm={6}
+                    md={6}
+                  >
+                    <FormLabel
+                      sx={{
+                        fontSize: {sm:"20px",md:"15px"},
+                        fontWeight: "600",
+                        color: "#3C3C3C",
+                        marginBottom: "7px",
+                        marginTop: {xs:"0px",sm:"22px",md:"22px"},
+                      }}
+                      required
+                    >
+                      Contact Person
+                    </FormLabel>
+                    <TextField
+                      InputProps={{ sx: { height: "37px", fontSize:{sm:"18px", md:"14px"} } }}
+                      placeholder="Contact Person"
+                    />
+                  </Grid>
+                </Grid>
               </FormControl>
             </Box>
           </Box>
-          {/* <Box>
-            <Typography>Back</Typography>
-            <Button variant="contained">Next</Button>
-          </Box> */}
+          <Box
+            sx={{
+              width: { xs: "100%", md: "85.5%" },
+              // border:"1px solid red",
+              padding:{xs:"0 15px", md:"0"},
+              margin: "auto",
+              display: "flex",
+              justifyContent: "space-between",
+              marginTop: "30px",
+              paddingBottom:{xs:"20px", md:"0"},
+            }}
+          >
+            <Button variant="text" sx={{ textTransform: "none",fontSize:{sm:"20px",md:"13px"} }} disabled>
+              Back
+            </Button>
+            <Button
+              variant="contained" size="small"
+              sx={{ textTransform: "none",fontSize:{sm:"20px",md:"13px"}, padding: "6px 8px" }}
+              endIcon={<ArrowForwardIcon />}
+            >
+              Next
+            </Button>
+          </Box>
         </Box>
       </Box>
     </Box>
